3aafe323332878d6532824fafb2af3cc8d17449f
RroAias <anthony@henix.be>
2024-12-19 17:40:00 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApp3/ColorSet.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class ColorSet
+    {
+        float yellow;
+        float red;
+        float blue;
+        float totalTime;
+
+        public void RunOnColor(string color, float seconds)
+        {
+            totalTime += seconds;
+
+            switch (color.ToLower())
+            {
+                case "yellow":
+                    yellow = yellow + (3 * seconds);
+                    if (yellow > 100) yellow = 100;
+
+                    red = red - seconds;
+                    if (red < 100) red = 0;
+
+                    blue = blue - seconds;
+                    if (blue < 100) blue = 0;
+                    break;
+
+                case "red":
+                    red = red + (3 * seconds);
+                    if (red > 100) red = 100;
+
+                    yellow = yellow - seconds;
+                    if (yellow < 0) yellow = 0;
+
+                    blue = blue - seconds;
+                    if (blue < 0) blue = 0;
+                    break;
+
+                case "blue":
+                    blue = blue + (3 * seconds);
+                    if (blue > 100) blue = 100;
+
+                    yellow = yellow - seconds;
+                    if (yellow < 0) yellow = 0;
+
+                    red = red - seconds;
+                    if (red < 0) red = 0;
+                    break;
+            }
+
+        }
+
+        public string GetColor()
+        {
+            bool hasYellow = yellow >= 3;
+            bool hasRed = red >= 3;
+            bool hasBlue = blue >= 3;
+
+            if (hasYellow && hasRed && hasBlue)
+                return "black";
+
+            if (hasYellow && hasRed)
+                return "orange";
+
+            if (hasRed && hasBlue)
+                return "purple";
+            if (hasYellow && hasBlue)
+                return "green";
+            if (hasYellow)
+                return "yellow";
+            if (hasRed)
+                return "red";
+            if (hasBlue)
+                return "blue";
+            return "none";
+        }
+        public (float Yellow, float Red, float Blue, float Time) GetStatus()
+        {
+            return (yellow, red, blue, totalTime);
+        }
+    }
+
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new ColorSet();
+
+            while (true)
+            {
+                Console.WriteLine("Sur quelle couleur voulez-vous courir ? (yellow, red, blue) ou tapez 'exit' pour quitter :");
+                string color = Console.ReadLine();
+                if (color.ToLower() == "exit")
+                { break; }
+                Console.WriteLine("Combien de secondes voulez-vous courir sur cette couleur ?");
+                if (float.TryParse(Console.ReadLine(), out float seconds)) {
+                    game.RunOnColor(color, seconds);
+                    var status = game.GetStatus();
+                    Console.WriteLine($"Jaune: {status.Yellow}, Rouge: {status.Red}, Bleu: {status.Blue}, Temps total: {status.Time} secondes");
+                    Console.WriteLine($"Couleur actuelle : {game.GetColor()}"); }
+                else
+                {
+                    Console.WriteLine("Veuillez entrer un nombre valide de secondes.");
+
+                }
+            }
+
+        }
+    }
+}
